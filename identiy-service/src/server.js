bf97659a0d948ded5abd526efa0680de72d2fe0f
Everest Paudel <paudeleverest09@gmail.com>
2025-09-17 00:38:03 +0545

    Identiy Service - Server

--- identiy-service/src/server.js
+++ identiy-service/src/server.js
@@ -2,11 +2,17 @@
  const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const express = require('express')
- const  app = express()
- const helmet = require('helmet')
- const cors = require('cors')
- const Redis = require('ioredis')
- const {RateLimiterRedis}= require('rate-limiter-flexible')
+const helmet = require('helmet')
+const cors = require('cors')
+const Redis = require('ioredis')
+const {RateLimiterRedis}= require('rate-limiter-flexible')
+const {rateLimit}= require('express-rate-limit')
+const {RedisStore}= require('rate-limit-redis')
+
+const routes = require('./routes/identity-service')
+const errorHandler = require('./middleware/errorHandler')
+const  app = express()
+const PORT =process.env.PORT || 3001
  //connect to mongodb
  mongoose.connect(process.env.MONGO_URI).then(
 
@@ -47,3 +53,56 @@ app.use((req ,res ,next)=>{
         })
     })
 })
+
+
+
+
+//Ip based Rate Limiting for sensentive endpoints
+
+const sensentiveEndPointsLimiter =  rateLimit({
+    windowMs : 15 *60 *1000,
+    max:50,
+    standardHeaders:true,
+    legacyHeaders:false,
+    handler:(req ,res)=>{
+        logger.warn(`Sensentive endPoint rate limits  exceed for I{: ${req.ip}}`),
+          res.status(429).json({
+            sucess:false,
+            message:'Too many Request'
+        })
+
+    },
+    store : new RedisStore({
+        sendCommand :(...args)=> redisClient.call(...args)
+    })
+})
+
+
+ //apply  this sensentive endpoint 
+
+ app.use('/api/auth/register',sensentiveEndPointsLimiter)
+
+
+ app.use('/api/auth',routes)
+
+ //error handler
+ app.use(errorHandler)
+
+
+ //started the server
+cosnt 
+ app.listen(PORT,()=>{
+    logger.info(`Identity service is runniing at ${PORT}`)
+
+
+ })
+
+
+
+ //Unhandled promise p
+ process.on('unhandledRejection',(reason ,promise)=>{
+ logger.error("Unhandled Rejection at ",promise ,"reason :",reason)
+
+ })
+ 
+
